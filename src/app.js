fd1aba858c11dd5d50c9a1b4b99a01652abac087
Jonathan Norestin <jnorestin@gmail.com>
2023-04-14 16:26:46 +0000

    randomcard

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("here");
+  document.querySelector(".card").innerHTML = randomNumber();
+  document.querySelector(".card").classList.add(randomSuits());
 };
+function randomNumber() {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let random = Math.floor(Math.random() * number.length);
+  return number[random];
+}
+function randomSuits() {
+  let suits = ["spades", "clubs", "hearts", "diamonds"];
+  let random = Math.floor(Math.random() * suits.length);
+  return suits[random];
+}
